46120b2a7641762b43164c33ec58d56e83cf2d77
Andrey Mironenko <qwerty.mironenko@gmail.com>
2023-02-17 17:09:35 +0300

    add Manual && TextRank v2

--- testing_keywords_mai.py
+++ testing_keywords_mai.py
@@ -8,6 +8,8 @@ from nltk.corpus import stopwords
 from sklearn.feature_extraction.text import TfidfVectorizer
 from summa import keywords
 
+from yake_keywords_mai import extract_keywords
+
 nltk.download("stopwords")
 with open("theses.json","r",encoding="utf8") as f:
     all_texts = json.load(f)
@@ -25,11 +27,14 @@ for i in list(all_texts):
     print(subject)
     print()
     print(text)
+    print("\n\nManual")
+    print(extract_keywords(text)["manual"])
     print("\n\nRAKE")
     stops = list(set(stopwords.words("russian")))
 
     rake = Rake (stopwords = stops, max_words = 3)
-    print(rake.apply(text)[:10])
+    # print(rake.apply(text)[:10]) # raw output
+    print([i[0] for i in rake.apply(text)[:10]])
     print("\n\nYAKE!")
     extractor = yake.KeywordExtractor (
         lan = "ru",     # язык
@@ -37,9 +42,10 @@ for i in list(all_texts):
         dedupLim = 0.3, # порог похожести слов
         top = 10        # количество ключевых слов
     )
+    # print(extractor.extract_keywords(text)) # raw output
     print([i[0] for i in extractor.extract_keywords(text)])
 
-    print("\n\nTextRank!")
+    print("\n\nTextRank")
     text_clean = ""
     # уберем стоп-слова
     for i in text.split():
@@ -49,7 +55,21 @@ for i in list(all_texts):
     from textblob import TextBlob
     blob = TextBlob(text_clean)
     noun = blob.noun_phrases
-    print("\n\nTfidfVectorizer!")
+
+    print("\n\nTextRank v2")
+    import pytextrank
+    import spacy
+
+    nlp = spacy.load("ru_core_news_sm")
+
+    # add PyTextRank to the spaCy pipeline
+    nlp.add_pipe("textrank")
+    doc = nlp(text)
+
+    # examine the top-ranked phrases in the document
+    print([phrase.text for phrase in doc._.phrases])
+
+    print("\n\nTfidfVectorizer")
     vectorizer = TfidfVectorizer()
     X = vectorizer.fit_transform(noun)
     from pymorphy3 import MorphAnalyzer
