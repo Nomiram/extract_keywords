e84f46e0738aa34d39dac2586e602d4e6b65ea29
Andrey Mironenko <qwerty.mironenko@gmail.com>
2023-03-05 00:13:25 +0300

    fix /bin/bash

--- pip-setup.sh
+++ pip-setup.sh
@@ -1,2 +1,2 @@
-#!usr/bin/bash
+#!/bin/bash
 python -m spacy download ru_core_news_sm
--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-#!usr/bin/bash
+#!/bin/bash
 pip3 install -r production_requirements.txt
 python3 setup.py
 python3 -m spacy download ru_core_news_sm
