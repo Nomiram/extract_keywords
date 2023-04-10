c43dbc462358c4aadf5a42f865ef78c9903fac2b
Andrey Mironenko <qwerty.mironenko@gmail.com>
2023-04-11 00:52:12 +0300

    add new method (TextRank v2)

---
+++ text_rank_v2_normal_form.py
@@ -0,0 +1,72 @@
+'''extract keywords from AiK thesis'''
+import json
+import sys
+
+import pymorphy3
+import pytextrank
+import spacy
+from nltk.corpus import stopwords
+from summa import keywords
+
+from manual_m import manual_m
+
+stop_words = ""
+with open("stop_words.json", "r", encoding="UTF8") as f:
+    stop_words = json.load(f)
+
+
+def TextRank_m(text: str):
+    """
+    The function takes in a string of text, uses spaCy and PyTextRank to extract key phrases, 
+    and then uses pymorphy3 to normalize the phrases before returning them as a list.
+
+    Args:
+      text (str): The input text that needs to be analyzed and extracted for keywords using 
+    the TextRank algorithm.
+
+    Returns:
+      List of top-ranked phrases in the input text after applying the TextRank algorithm. 
+    The returned list contains the normalized form of the phrases.
+    """
+
+    nlp = spacy.load("ru_core_news_sm")
+
+    # add PyTextRank to the spaCy pipeline
+    nlp.add_pipe("textrank")
+
+    # TEXT RANK
+    doc = nlp(text)
+
+    # examine the top-ranked phrases in the document
+    raw_text_rank_keywords2 = keywords.keywords(
+        text, language="russian", words=10, additional_stopwords=stop_words).split()
+    # text, language="russian", words=10, additional_stopwords=stopwords.words("russian")).split()
+    # raw_text_rank_keywords2 = [phrase.text for phrase in doc._.phrases]
+
+    morph = pymorphy3.MorphAnalyzer()
+    text_rank_keywords2 = []
+    for phrase in raw_text_rank_keywords2:
+        words = phrase.split()
+        ready_words = []
+        for word in words:
+            parsed_word = morph.parse(word)[0]
+            ready_words.append(parsed_word.normal_form)
+
+        text_rank_keywords2.append(
+            pymorphy3.shapes.restore_capitalization(" ".join(ready_words), phrase))
+    return list(set(text_rank_keywords2))
+
+
+def extract_keywords(text):
+    '''extract keywords by `TextRank`'''
+    result = manual_m(text)
+    text_rank_result = TextRank_m(text)
+    return {"manual": result, "TextRank": text_rank_result}
+
+
+if __name__ == "__main__":
+    # nltk.download("stopwords", quiet=True)
+    if not pytextrank.version:
+        sys.exit()
+    input_text = sys.stdin.readlines()
+    print(json.dumps(extract_keywords("\n".join(input_text))))
